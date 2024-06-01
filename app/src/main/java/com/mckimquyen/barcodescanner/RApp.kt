d8b187b9423424b3e9c7f6e0680be157a49ff29d
roy <roy93group@gmail.com>
2024-06-01 20:28:38 +0700

    update

--- app/src/main/java/com/mckimquyen/barcodescanner/RApp.kt
+++ app/src/main/java/com/mckimquyen/barcodescanner/RApp.kt
@@ -5,6 +5,24 @@ import com.mckimquyen.barcodescanner.di.settings
 import com.mckimquyen.barcodescanner.usecase.Logger
 import io.reactivex.plugins.RxJavaPlugins
 
+
+//TODO ad applovin
+//TODO finger print
+//TODO why you see ad
+//TODO import oplm vietnam
+
+//TODO change icon launcher
+//TODO double to exit app
+//TODO leak canary
+//TODO proguard
+//TODO change pkg name manifest
+//TODO policy
+//TODO rate app, share app, more app
+//TODO build version
+//TODO keystore
+
+//done mckimquyen
+
 class RApp : MultiDexApplication() {
 
     override fun onCreate() {
