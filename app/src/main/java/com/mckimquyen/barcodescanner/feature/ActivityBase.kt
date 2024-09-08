297a5f8b16410d5defeaa1d04762baeeab958b68
roy <loitp@galaxyjoy.vn>
2024-09-08 13:23:21 +0700

    update

--- app/src/main/java/com/mckimquyen/barcodescanner/feature/ActivityBase.kt
+++ app/src/main/java/com/mckimquyen/barcodescanner/feature/ActivityBase.kt
@@ -1,5 +1,7 @@
 package com.mckimquyen.barcodescanner.feature
 
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -12,5 +14,12 @@ abstract class ActivityBase : AppCompatActivity() {
         window.decorView.systemUiVisibility =
             window.decorView.systemUiVisibility or View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
         rotationHelper.lockCurrentOrientationIfNeeded(this)
+    }
+
+    override fun attachBaseContext(context: Context) {
+        val override = Configuration(context.resources.configuration)
+        override.fontScale = 1.0f
+        applyOverrideConfiguration(override)
+        super.attachBaseContext(context)
     }
 }
