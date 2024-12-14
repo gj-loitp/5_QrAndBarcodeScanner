50753f010626f84c7e38dcd184664a2b9fdd6dfb
roy <loitp@galaxyjoy.vn>
2024-12-14 16:08:39 +0700

    update

--- app/src/main/java/com/mckimquyen/barcodescanner/RApp.kt
+++ app/src/main/java/com/mckimquyen/barcodescanner/RApp.kt
@@ -10,10 +10,11 @@ import io.reactivex.plugins.RxJavaPlugins
 //TODO roy93~ why you see ad
 //TODO roy93~ join beta tester
 //TODO roy93~ khi scan image no ko work doi voi hinh anh do chinh app nay tao ra
-//TODO roy93~ 120hz
-//TODO roy93~ review in app
 
 //done mckimquyen
+//font scale
+//120hz
+//review in app
 //ad applovin
 //github
 //policy
--- app/src/main/java/com/mckimquyen/barcodescanner/feature/ActivityBase.kt
+++ app/src/main/java/com/mckimquyen/barcodescanner/feature/ActivityBase.kt
@@ -1,11 +1,22 @@
 package com.mckimquyen.barcodescanner.feature
 
+import android.app.Activity
 import android.content.Context
 import android.content.res.Configuration
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.Display
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.review.ReviewException
+import com.google.android.play.core.review.ReviewInfo
+import com.google.android.play.core.review.ReviewManagerFactory
+import com.google.android.play.core.review.model.ReviewErrorCode
+import com.mckimquyen.barcodescanner.BuildConfig
 import com.mckimquyen.barcodescanner.di.rotationHelper
+import java.util.Calendar
 
 abstract class ActivityBase : AppCompatActivity() {
 
@@ -21,5 +32,70 @@ abstract class ActivityBase : AppCompatActivity() {
         override.fontScale = 1.0f
         applyOverrideConfiguration(override)
         super.attachBaseContext(context)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            enableAdaptiveRefreshRate()
+        }
+        rateAppInApp(BuildConfig.DEBUG)
+    }
+
+    private fun enableAdaptiveRefreshRate() {
+        val wm = getSystemService(WINDOW_SERVICE) as WindowManager
+        val display: Display? = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            display // Sử dụng API mới
+        } else {
+            @Suppress("DEPRECATION")
+            wm.defaultDisplay // Fallback cho API thấp hơn
+        }
+
+        if (display != null) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                val supportedModes = display.supportedModes
+                val highestRefreshRateMode = supportedModes.maxByOrNull { it.refreshRate }
+                if (highestRefreshRateMode != null) {
+                    window.attributes = window.attributes.apply {
+                        preferredDisplayModeId = highestRefreshRateMode.modeId
+                    }
+                    println("Adaptive refresh rate applied: ${highestRefreshRateMode.refreshRate} Hz")
+                }
+            }
+        }
+    }
+}
+
+fun Activity.rateAppInApp(forceRateInApp: Boolean = false) {
+    //import gradle app
+//    implementation("com.google.android.play:review:2.0.2")
+//    implementation("com.google.android.play:review-ktx:2.0.2")
+
+    val sharedPreferences = getSharedPreferences("app_preferences", Context.MODE_PRIVATE)
+    val lastReviewTime = sharedPreferences.getLong("last_review_time", 0L)
+    Log.d("roy93~", "requestReview lastReviewTime $lastReviewTime")
+    val currentTime = Calendar.getInstance().timeInMillis
+    val daysSinceLastReview = (currentTime - lastReviewTime) / (1000 * 60 * 60 * 24)
+    Log.d("roy93~", "requestReview forceRateInApp $forceRateInApp")
+    Log.d("roy93~", "requestReview daysSinceLastReview $daysSinceLastReview")
+    if (daysSinceLastReview >= 7 || forceRateInApp) {
+//    if (daysSinceLastReview >= 7) {
+        val reviewManager = ReviewManagerFactory.create(this)
+        val request = reviewManager.requestReviewFlow()
+        request.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val reviewInfo: ReviewInfo = task.result
+                reviewManager.launchReviewFlow(this, reviewInfo)
+                sharedPreferences.edit().putLong("last_review_time", currentTime).apply()
+                Log.d("roy93~", "requestReview result ${task.result}")
+                Log.d("roy93~", "requestReview isSuccessful ${task.isSuccessful}")
+                Log.d("roy93~", "requestReview isCanceled ${task.isCanceled}")
+                Log.d("roy93~", "requestReview isComplete ${task.isComplete}")
+                Log.d("roy93~", "requestReview exception ${task.exception}")
+            } else {
+                @ReviewErrorCode val reviewErrorCode = (task.exception as ReviewException?)?.errorCode
+                Log.e("roy93~", "requestReview error $reviewErrorCode")
+            }
+        }
     }
 }
